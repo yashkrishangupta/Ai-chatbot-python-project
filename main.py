e278e200544bbf551b31f6599ddd87844d064c7e
yashkrishangupta <yashkrishangupta@gmail.com>
2025-03-29 19:26:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from app import app
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=port, debug=True, use_reloader=False)
+    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)
