ea2f10a0c1c766c10302cff4fc91a1f02818a846
yashkrishangupta <yashkrishangupta@gmail.com>
2025-04-02 18:07:55 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -10,7 +10,7 @@ class ChatBot:
         # Configure Gemini API
         api_key = "AIzaSyA9Q4j0JqXyR50Xj-uYgfM49nbQoMg7pMs"  
         genai.configure(api_key=api_key)
-        self.model = genai.GenerativeModel("Gemini 1.5") 
+        self.model = genai.GenerativeModel("gemini-pro") 
         logging.debug("ChatBot initialized with Gemini API.")
 
     def get_response(self, user_input):
